38149ec8d1079bc62401d33b2c8c5b12477c8b5a
pedro jonathan <jonathan84376008@gmail.com>
2026-06-12 20:40:53 -0300

    Restaura versao correta da calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,35 +1,43 @@
-def main(): # define a função main, que é a função que executa o progama 
-    print('CALCULADORA DO JONATHAN')
+def main(): # criei uma função junto ao comando 'main()' para organizar o codigo
+    print("CALCULADORA DO JONATHAN") # pedi pro python exibir o nome da minha calculadora
+
+    while True: # iniciei um loop para voltar para o menu sempre depois que finalizar o calculo
+        print("\n1- Soma")
+        print("2- Subtração")
+        print("3- Multiplicação")
+        print("4- Divisão")
+        print("5- Potência")
+        print("6- Sair")
+
+        escolha = int(input("Escolha uma opção: ")) # criei uma variavel com um tipo primitivo 'int'  que vai executar uma entrada via 'input' 
+
+        if escolha == 6: # criei um comando 'if' junto com um variavel e 'break(finaliza o loop)' para se escolher '6' sair
+            print("Thanks guys")
+            break
+
+        if escolha < 1 or escolha > 6: # iniciei um comando para o usuario escolher de 1 a 6 no meu menu, se a escolha for menor que 1 e maior que 6 o print vai exibir:
+            print("Opção inválida!")
+            continue
+
+        num1 = int(input("Digite um número: ")) # dei opções para o usuario digitar um numero do tipo primitivo 'inteiro' para calcular
+        num2 = int(input("Digite outro número: "))
+
+        if escolha == 1:
+            print("Resultado:", num1 + num2) # criei um comando para executar a conta 'se isso for verdadeiro execute' com if
+
+        elif escolha == 2: # criei um comando com ' senão, se issofor verdadeiro' com o elif para criar varias opções
+            print("Resultado:", num1 - num2)
+
+        elif escolha == 3:
+            print("Resultado:", num1 * num2)
+
+        elif escolha == 4:
+            print("Resultado:", num1 / num2)
+
+        elif escolha == 5:
+            print("Resultado:", num1 ** num2)
+
+
+main() # rodei o progama fechando com o 'main()'
+
     
-    num1=int(input('Digite um numero: '))
-    num2=int(input('Digite outro numero: '))
-
-# estou pedindo para o python ler um numero inteiro e armazenar na memoria, e depois ler outro numero inteiro e armazenar para depois somar, subtrair, multiplicar ou dividir.
-    
-
-    print('1- soma')
-    print('2- subtração')
-    print('3- multiplicação')
-    print('4- divisão')
-    print('5- sair')
-
-    # dei opçoes para o usuario escolher qual operação fazer usando o print, e depois armazenando a escolha na memoria para depois usar o if
-
-    escolha = int(input('Escolha a operação que deseja realizar: '))
-
-    if escolha == 1:  # se a escolha for 1, ele vai executar o bloco de codigo abaixo por causa do if
-        print('A soma é: ', num1 + num2)
-    elif escolha == 2: # se a escolha for 2, ele vai executar o bloco de codigo abaixo por causa do elif
-        print('A subtração é: ', num1 - num2)
-    elif escolha == 3: 
-        print('A multiplicação é: ', num1 * num2) # * é o simbolo de multiplicação
-    elif escolha == 4:
-        print('A divisão é: ', num1 / num2) # / é o simbolo de divisão
-    elif escolha == 5:
-        print('THANKS GUYS')
-
-
-main() # executa a função main para iniciar o progama
-   
-    
-
